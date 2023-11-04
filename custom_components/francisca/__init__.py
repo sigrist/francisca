60527e443e0d07279776426a2f754d84aa3230d2
Paulo Sigrist <paulo.sigrist@gmail.com>
2023-11-04 10:30:09 -0300

    Requirements

--- custom_components/francisca/__init__.py
+++ custom_components/francisca/__init__.py
@@ -1,14 +1,10 @@
 from homeassistant.helpers.typing import HomeAssistantType
-import logging
 
 DOMAIN = "francisca"
 
 
 async def setup(hass: HomeAssistantType, config: dict):
     hass.states.async_set(f"{DOMAIN}.state", "on")
-    entity = config["entity"]
-
-    logging.info(f"Entity: {entity}")
 
     # Return boolean to indicate that initialization was successful.
     return True
